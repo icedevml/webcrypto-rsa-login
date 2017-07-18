124ca5c23d32d777b99dbad1cf74acaf40e5159e
Michał Leszczyński <m.leszczynski.6@stud.elka.pw.edu.pl>
2017-07-18 02:54:16 +0200

    distinct ids for login and register form, added checks in .js files

--- static/login.js
+++ static/login.js
@@ -1,24 +1,28 @@
-callIndexedDB(function (store) {
-    var getData = store.get("key_pair");
-    getData.onsuccess = function () {
-        if (!getData.result) {
-            document.getElementById('form').innerHTML = 'You don\'t have any key generated yet.';
-            return;
-        }
+var login_form = document.getElementById('login_form');
 
-        // we've fetched our key pair (which was generated during registration) from IndexedDB
-        var publicKey = getData.result.publicKey;
-        var privateKey = getData.result.privateKey;
-        var challenge = base64ToBinary(document.getElementById('challenge').value);
+if (login_form) {
+    callIndexedDB(function (store) {
+        var getData = store.get("key_pair");
+        getData.onsuccess = function () {
+            if (!getData.result) {
+                document.getElementById('form').innerHTML = 'You don\'t have any key generated yet.';
+                return;
+            }
 
-        exportKey('spki', publicKey).then(function (exportedKey) {
-            // here we export public key only in order to display it to the user
-            document.getElementById('public_key').value = spkiToPEM(exportedKey);
-            return signData(challenge, privateKey);
-        }).then(function (signed) {
-            // fill the "signature" form field with the signature of the challenge
-            // it will be sent over to the server for verification
-            document.getElementById('signature').value = arrayToHex(signed);
-        });
-    };
-});
+            // we've fetched our key pair (which was generated during registration) from IndexedDB
+            var publicKey = getData.result.publicKey;
+            var privateKey = getData.result.privateKey;
+            var challenge = base64ToBinary(document.getElementById('challenge').value);
+
+            exportKey('spki', publicKey).then(function (exportedKey) {
+                // here we export public key only in order to display it to the user
+                document.getElementById('public_key').value = spkiToPEM(exportedKey);
+                return signData(challenge, privateKey);
+            }).then(function (signed) {
+                // fill the "signature" form field with the signature of the challenge
+                // it will be sent over to the server for verification
+                document.getElementById('signature').value = arrayToHex(signed);
+            });
+        };
+    });
+}
--- static/register.js
+++ static/register.js
@@ -1,28 +1,32 @@
-makeKeys().then(function (keys) {
-    exportKey('spki', keys.publicKey).then(function (exportedKey) {
-        document.getElementById('public_key').value = spkiToPEM(exportedKey);
+var register_form = document.getElementById('register_form');
 
-        // prepare a structure similiar to PKAC (Public Key and Challenge)
+if (register_form) {
+    makeKeys().then(function (keys) {
+        exportKey('spki', keys.publicKey).then(function (exportedKey) {
+            document.getElementById('public_key').value = spkiToPEM(exportedKey);
 
-        // we use a challenge here so the user won't be able to send a public key
-        // which belongs to somebody else and thus grant access to the account
-        var challenge = base64ToBinary(document.getElementById('challenge').value);
-        var pkac = new Uint8Array(exportedKey.byteLength + challenge.byteLength);
-        pkac.set(new Uint8Array(exportedKey), 0);
-        pkac.set(new Uint8Array(challenge), exportedKey.byteLength);
+            // prepare a structure similiar to PKAC (Public Key and Challenge)
 
-        // we are not going to sign "user name" field here, if somebody is able to
-        // change it on the web application level then he would be also able to generate
-        // a proper signature
+            // we use a challenge here so the user won't be able to send a public key
+            // which belongs to somebody else and thus grant access to the account
+            var challenge = base64ToBinary(document.getElementById('challenge').value);
+            var pkac = new Uint8Array(exportedKey.byteLength + challenge.byteLength);
+            pkac.set(new Uint8Array(exportedKey), 0);
+            pkac.set(new Uint8Array(challenge), exportedKey.byteLength);
 
-        // sign data (so we have a signed "PKAC")
-        return signData(pkac, keys.privateKey);
-    }).then(function (signed) {
-        document.getElementById('signature').value = arrayToHex(signed);
+            // we are not going to sign "user name" field here, if somebody is able to
+            // change it on the web application level then he would be also able to generate
+            // a proper signature
 
-        // securely store the new key pair in the IndexedDB
-        callIndexedDB(function (store) {
-            store.put({id: "key_pair", publicKey: keys.publicKey, privateKey: keys.privateKey});
+            // sign data (so we have a signed "PKAC")
+            return signData(pkac, keys.privateKey);
+        }).then(function (signed) {
+            document.getElementById('signature').value = arrayToHex(signed);
+
+            // securely store the new key pair in the IndexedDB
+            callIndexedDB(function (store) {
+                store.put({id: "key_pair", publicKey: keys.publicKey, privateKey: keys.privateKey});
+            });
         });
     });
-});
+}
