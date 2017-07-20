a656f20c95cb8071fe348307c93cb6b7995e48a1
Michał Leszczyński <m.leszczynski.6@stud.elka.pw.edu.pl>
2017-07-20 22:45:29 +0200

    decoupled form logic and signing logic

---
+++ static/signing.js
@@ -0,0 +1,89 @@
+function performLogin(challenge) {
+    if (typeof challenge === "string") {
+        challenge = base64ToBinary(challenge);
+    }
+
+    return callIndexedDB(function (store) {
+        return new Promise(function (resolve, reject) {
+            var getData = store.get("key_pair");
+            getData.onsuccess = function () {
+                // we've fetched our key pair (which was generated during registration) from IndexedDB
+                resolve(getData.result);
+            };
+            getData.onerror = function (err) {
+                reject(err);
+            };
+        });
+    }).then(function (dbResult) {
+        if (!dbResult) {
+            return Promise.reject('no_key_stored');
+        }
+
+        var exportKeyPromise = exportKey('spki', dbResult.publicKey);
+        var signDataPromise = signData(challenge, dbResult.privateKey);
+
+        return Promise.all([exportKeyPromise, signDataPromise]);
+    }).then(function (vals) {
+        var exportedKey = vals[0];
+        var rawSignature = vals[1];
+
+        // here we export public key only in order to display it to the user
+        // it's not required in the login process, as the server needs to know public key already
+        var publicKey = spkiToPEM(exportedKey);
+
+        // fill the "signature" form field with the signature of the challenge
+        // it will be sent over to the server for verification
+        var signature = arrayToHex(rawSignature);
+
+        return {"publicKey": publicKey, "signature": signature};
+    });
+}
+
+function performRegister(challenge) {
+    if (typeof challenge === "string") {
+        challenge = base64ToBinary(challenge);
+    }
+
+    return makeKeys().then(function (keys) {
+        return Promise.all([keys, exportKey('spki', keys.publicKey)]);
+    }).then(function (vals) {
+        var keys = vals[0];
+        var exportedKey = vals[1];
+
+        var publicKeyPEM = spkiToPEM(exportedKey);
+
+        // prepare a structure similiar to PKAC (Public Key and Challenge)
+
+        // we use a challenge here so the user won't be able to send a public key
+        // which belongs to somebody else and thus grant access to the account
+        var pkac = new Uint8Array(exportedKey.byteLength + challenge.byteLength);
+        pkac.set(new Uint8Array(exportedKey), 0);
+        pkac.set(new Uint8Array(challenge), exportedKey.byteLength);
+
+        // we are not going to sign "user name" field here, if somebody is able to
+        // change it on the web application level then he would be also able to generate
+        // a proper signature
+
+        // sign data (so we have a signed "PKAC")
+        return Promise.all([keys, publicKeyPEM, signData(pkac, keys.privateKey)]);
+    }).then(function (vals) {
+        var keys = vals[0];
+
+        // securely store the new key pair in the IndexedDB
+        var dbPromise = callIndexedDB(function (store) {
+            return new Promise(function (resolve, reject) {
+                store.put({id: "key_pair", publicKey: keys.publicKey, privateKey: keys.privateKey});
+                resolve();
+            });
+        });
+
+        vals.push(dbPromise);
+        return Promise.all(vals);
+    }).then(function (vals) {
+        var keys = vals[0];
+        var publicKeyPEM = vals[1];
+        var signature = arrayToHex(vals[2]);
+
+        return {"publicKey": publicKeyPEM, "signature": signature};
+    });
+}
